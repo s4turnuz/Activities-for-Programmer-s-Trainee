950cec70daaedeba5c2c773933451ac50ea346e2
Douglas Sammur <douglas.sammur@programmers.com.br>
2022-05-17 08:26:40 -0300

    Separacao de atvs

---
+++ operadores/Atv1/Atv1.cs
@@ -0,0 +1,7 @@
+﻿// EXERCÍCIO N1:
+// ==> Programa para a Média de Idade
+int IdadeMinima = 20;
+int IdadeMaxima = 50;
+int IdadeMedia = (IdadeMinima+IdadeMaxima)/2; 
+
+Console.WriteLine($"A media de idade é {IdadeMedia} anos");
